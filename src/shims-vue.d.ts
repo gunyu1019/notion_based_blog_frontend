ee5871fa9ec5b1e9fb12bd208d63e6fbcc5e27ad
gunyu1019 <gunyu1019@gmail.com>
2023-09-29 21:10:20 +0900

    [Init] add shims-scss.d.ts and shims-vue.d.ts

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,10 +1,5 @@
 declare module '*.vue' {
-    import type { DefineComponent } from 'vue'
-    const component: DefineComponent<{}, {}, any>
-    export default component
-}
-
-declare module '*.scss' {
-    const content: {[className: string]: string};
-    export default content;
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
 }
