44af0cec8b26d12425df6903fcf314f864c7137c
gunyu1019 <gunyu1019@gmail.com>
2024-01-07 01:12:48 +0900

    [Feat] Add Styled Text Component

---
+++ src/components/StyledText.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+import type RichText from '@/types/RichText'
+
+defineProps<{
+    richText: RichText
+}>()
+</script>
+
+<template>
+    <a v-if="richText.href !== null" v-bind:href="richText.href">
+        <span
+            v-bind:style="{
+                textDecoration: `${richText.underline === true ? 'underline' : ''}
+                ${richText.strikethrough === true ? 'line-through' : ''}`,
+                fontWeight: richText.bold === true ? 'bold' : null,
+                fontStyle: richText.italic ? 'italic' : 'normal',
+                color: richText.color
+            }"
+            >{{ richText.text }}</span
+        >
+    </a>
+    <span
+        v-else
+        v-bind:style="{
+            textDecoration: `${richText.underline === true ? 'underline' : ''}
+                ${richText.strikethrough === true ? 'line-through' : ''}`,
+            fontWeight: richText.bold === true ? 'bold' : null,
+            fontStyle: richText.italic ? 'italic' : 'normal',
+            color: richText.color
+        }"
+        >{{ richText.text }}</span
+    >
+</template>
+
+<style scoped></style>
