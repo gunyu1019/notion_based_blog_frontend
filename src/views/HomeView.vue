dc170edc5cffd4da884579f6de76561e081235d5
gunyu1019 <gunyu1019@gmail.com>
2024-01-07 01:14:01 +0900

    [Feat] Add Navbar Header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import StyledText from '@/components/StyledText.vue'
+import type RichText from '@/types/RichText'
+
+const richText: RichText = {
+    text: '안녕세상?',
+    href: 'https://yhs.kr',
+    strikethrough: false,
+    underline: false,
+    bold: false,
+    color: 'white'
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+        <StyledText :rich-text="richText" />
+    </main>
 </template>
