1324942ed42e828b9a750407d08e0f63946ba145
gunyu1019 <gunyu1019@gmail.com>
2024-01-07 01:12:38 +0900

    [Feat] Add bootstrap-vue-next

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,9 @@
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
+    import type { DefineComponent } from 'vue'
+    const component: DefineComponent<{}, {}, any>
+    export default component
+}
+
+declare module 'vue/types/vue' {
+    interface Vue {}
 }
