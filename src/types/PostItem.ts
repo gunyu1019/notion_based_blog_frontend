50e3633dcc2598ed1855d2791e59b3d212076bc5
gunyu1019 <gunyu1019@gmail.com>
2024-01-14 02:27:08 +0900

    [Feat] Add type of PostItem and Category

---
+++ src/types/PostItem.ts
@@ -0,0 +1,10 @@
+import Category from './Category'
+
+export default interface PostItem {
+    id: String
+    title: String
+    published_at: Date
+    thumbnail_url: String
+    hits: bigint
+    category: Category[]
+}
