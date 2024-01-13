005948236d4be4599bb6e7839d3f61182f4c634a
gunyu1019 <gunyu1019@gmail.com>
2024-01-14 02:26:49 +0900

    [Feat] (Temporaire) Add proxy at vite.config.ts for CORS

--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,14 @@ export default defineConfig({
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
+    },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8000',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, '')
+            }
+        }
     }
 })
