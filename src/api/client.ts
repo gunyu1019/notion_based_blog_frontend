1f61259beb0c671de91e9898297820d6921da7b0
gunyu1019 <gunyu1019@gmail.com>
2024-11-09 00:51:46 +0000

    [Feat] (WIP) Add getPost method

--- src/api/client.ts
+++ src/api/client.ts
@@ -5,7 +5,7 @@ import type PostItem from '@/types/PostItem'
 
 export default class Client extends AxiosService {
     constructor() {
-        super('/api')
+        super('https://super-duper-space-yodel-4jrrq45vxvj2jxx-8000.app.github.dev/')
     }
 
     request = async (config: AxiosRequestConfig) => {
@@ -15,10 +15,21 @@ export default class Client extends AxiosService {
         }
         return response.data
     }
-    getPosts = async (): PostItem => {
+    
+    getPosts = async (): Promise<PostItem[]> => {
         return await this.request({
             method: 'GET',
-            url: '/posts/'
+            url: '/posts'
+        })
+    }
+    
+    getPost = async (post_id: string): Promise<Post> => {
+        return await this.request({
+            method: 'GET',
+            url: '/post',
+            params: {
+                'post_id': post_id
+            }
         })
     }
 }
