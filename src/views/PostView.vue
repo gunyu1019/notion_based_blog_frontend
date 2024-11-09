decb817ad7caf91561861e69d9008d0085bde574
gunyu1019 <gunyu1019@gmail.com>
2024-11-09 00:51:59 +0000

    [Feat] (WIP) Add PostView

---
+++ src/views/PostView.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import Wave from '@/components/WaveItem.vue'
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+onMounted(() => {
+    console.log(route.query)
+})
+</script>
+
+<template>
+    <div class="wrapper"></div>
+    <Wave type="1" />
+    <div class="wrapper" style="background: #373b3e"></div>
+</template>
+
+<style lang="scss" scoped>
+div.wrapper {
+    padding: 100px 0;
+}
+</style>
